ee418c128cdb7c3938bfbc7fd09fa97a2d35fff8
zheng.li <zheng.li@xbongbong.com>
2022-06-20 19:19:44 +0800

    fix: @Bean

--- mbatis-hot-delpoy-spring-boot-starter/src/main/java/org/hot/batis/gray/FeignConfig.java
+++ mbatis-hot-delpoy-spring-boot-starter/src/main/java/org/hot/batis/gray/FeignConfig.java
@@ -1,11 +1,13 @@
 package org.hot.batis.gray;
 
 import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
 
+    @Bean
     public RequestInterceptor requestInterceptor() {
         return new FeignInterceptor();
     }
